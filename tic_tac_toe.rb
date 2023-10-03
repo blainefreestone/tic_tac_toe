291f961a750bc8bb79d410fa763d1ff8ad31f6fb
blainefreestone <blainefreestone01@gmail.com>
2023-10-03 13:58:42 -0700

    Modulized the functionality to detect if game is over. Making moves is now 2 players.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -28,19 +28,10 @@ class Board
 
   private
 
-  def column_arrays
-    @board_status.reduce(Array.new(3) { [] }) do |column_arrays, row_array|
-      column_arrays[0].push(row_array[0])
-      column_arrays[1].push(row_array[1])
-      column_arrays[2].push(row_array[2])
-      column_arrays
-    end
-  end
-
-  def completed_row?
+  def completed_array?(array)
     is_complete = false
 
-    @board_status.each do |row_array|
+    array.each do |row_array|
       if row_array.all? { |row_value| row_value == 'x' } || row_array.all? { |row_value| row_value == 'o' }
         is_complete = true
       end
@@ -48,27 +39,27 @@ class Board
     is_complete
   end
 
-  def completed_column?
-    is_complete = false
+  def completed_row?
+    completed_array?(@board_status)
+  end
 
-    column_arrays.each do |column_array|
-      if column_array.all? { |column_value| column_value == 'x' } || column_array.all? { |column_value| column_value == 'o' }
-        is_complete = true
-      end
+  def completed_column?
+    column_arrays = @board_status.reduce(Array.new(3) { [] }) do |result_array, row_array|
+      result_array[0].push(row_array[0])
+      result_array[1].push(row_array[1])
+      result_array[2].push(row_array[2])
+      result_array
     end
-    is_complete
+
+    completed_array?(column_arrays)
   end
 
   def completed_diagonal?
-    if @board_status[0][0] == 'x' && @board_status[1][1] == 'x' && @board_status[2][2] == 'x'
-      true
-    elsif @board_status[0][0] == 'o' && @board_status[1][1] == 'o' && @board_status[2][2] == 'o'
-      true
-    elsif @board_status[0][2] == 'x' && @board_status[1][1] == 'x' && @board_status[2][0] == 'x'
-      true
-    elsif @board_status[0][2] == 'o' && @board_status[1][1] == 'o' && @board_status[2][0] == 'o'
-      true
-    else false end
+    diagonal_down = [@board_status[0][0], @board_status[1][1], @board_status[2][2]]
+    diagonal_up = [@board_status[0][2], @board_status[1][1], @board_status[2][0]]
+    diagonal_arrays = [diagonal_down, diagonal_up]
+
+    completed_array?(diagonal_arrays)
   end
 end
 
@@ -82,6 +73,8 @@ class Game
     until @board.game_over?
       puts @board
       make_move(1)
+      puts @board
+      make_move(2)
     end
     puts @board
     puts "Game over!"
@@ -94,8 +87,8 @@ class Game
       row = gets.to_i - 1
       puts 'Now type the column number where you would like to play'
       column = gets.to_i - 1
-      if @board.board_status[row, column]
-        @board.add_x(row, column)
+      unless @board.board_status[row, column] == '-'
+        player_number == 1 ? @board.add_x(row, column) : @board.add_o(row, column)
         made_move = true
       end
     end
