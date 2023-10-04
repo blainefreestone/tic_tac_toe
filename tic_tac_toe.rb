e0b9a5e59ff322f9d77ea1352ca107dad9608dc8
blainefreestone <blainefreestone01@gmail.com>
2023-10-04 16:05:17 -0700

    Methods #player_won? and #board_empty? added to check game status. Old private methods for checking if game was over removed and replaced by logic in #player_won?

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,60 +12,24 @@ class Board
     column_seperator = ' | '
     rows = LINES[0..2]
 
-    position_label = ->(position) { @board_status[position] || '-' }
+    position_label = ->(position) { @board_status[position] || position }
     full_row = ->(row) { row.map(&position_label).join(column_seperator) }
 
     ' ' + rows.map(&full_row).join(" \n" + row_seperator + "\n ")
   end
 
-  def add_marker(row_index, column_index, marker_type = 'x')
-    @board_status[row_index][column_index] = marker_type
+  def add_marker(position, marker_type)
+    @board_status[position] = marker_type
   end
 
-  def game_over?
-    completed_row? || completed_column? || completed_diagonal?
-  end
-
-  private
-
-  def completed_array?(array)
-    is_complete = false
-
-    array.each do |row_array|
-      if row_array.all? { |row_value| row_value == 'x' } || row_array.all? { |row_value| row_value == 'o' }
-        is_complete = true
-      end
+  def player_won?(player)
+    LINES.any? do |line|
+      line.all { |position| @board_status[position] == player.marker }
     end
-    is_complete
   end
 
-  def completed_row?
-    completed_array?(@board_status)
-  end
-
-  def completed_column?
-    # column_arrays = @board_status.reduce(Array.new(3) { [] }) do |result_array, row_array|
-    #   result_array[0].push(row_array[0])
-    #   result_array[1].push(row_array[1])
-    #   result_array[2].push(row_array[2])
-    #   result_array
-    # end
-
-    column_arrays = @board_status.each_with_object(Array.new(3) { [] }) do |row_array, result_array|
-      result_array[0].push(row_array[0])
-      result_array[1].push(row_array[1])
-      result_array[2].push(row_array[2])
-    end
-
-    completed_array?(column_arrays)
-  end
-
-  def completed_diagonal?
-    diagonal_down = [@board_status[0][0], @board_status[1][1], @board_status[2][2]]
-    diagonal_up = [@board_status[0][2], @board_status[1][1], @board_status[2][0]]
-    diagonal_arrays = [diagonal_down, diagonal_up]
-
-    completed_array?(diagonal_arrays)
+  def board_full?
+    @board_status[1..9].none?(&:!)
   end
 end
 
@@ -103,3 +67,4 @@ end
 
 board = Board.new
 puts board.board_in_text
+puts board.board_full?
